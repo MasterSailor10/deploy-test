61ef60536cc902bd393899cfed8e22a60f44ee71
Siddhartha Singh <144660011+MasterSailor10@users.noreply.github.com>
2026-04-17 16:13:19 +0530

    Update Profile.js

--- frontend/src/userSideRoutes/Profile.js
+++ frontend/src/userSideRoutes/Profile.js
@@ -58,7 +58,7 @@ class Profile extends Component {
       const token = localStorage.getItem('token');
       const user = JSON.parse(localStorage.getItem('user'));
 
-      const response = await axios.put(`https://library-management-system-velocity.onrender.com/${user.id}`,{ name: this.state.name, email: this.state.email },
+      const response = await axios.put(`https://library-management-system-velocity.onrender.com/profile/${user.id}`,{ name: this.state.name, email: this.state.email },
         { headers: { Authorization: `Bearer ${token}` } }
       );
 
