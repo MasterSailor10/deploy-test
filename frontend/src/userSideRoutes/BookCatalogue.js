b55f82014c2264e4572468d8073a846bcac61526
Siddhartha Singh <144660011+MasterSailor10@users.noreply.github.com>
2026-04-17 15:56:11 +0530

    Update BookCatalogue.js

--- frontend/src/userSideRoutes/BookCatalogue.js
+++ frontend/src/userSideRoutes/BookCatalogue.js
@@ -18,7 +18,7 @@ class BookCatalogue extends Component {
         try {
             const token = localStorage.getItem('token');
 
-            const response = await axios.get('https://library-management-system-velocity.onrender.com//books', {
+            const response = await axios.get('https://library-management-system-velocity.onrender.com/books', {
             headers: {
                 Authorization: `Bearer ${token}`
             }
